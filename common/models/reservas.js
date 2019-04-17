d133478d105cb34d2eac45426ffe048afae701c2
elizaal <elizaalcantara@poli.ufrj.br>
2019-04-17 20:44:38 -0300

    valor e duracao - OK

--- common/models/reservas.js
+++ common/models/reservas.js
@@ -1,4 +1,18 @@
+/* eslint-disable indent */
 'use strict';
 
 module.exports = function(Reservas) {
+Reservas.observe('before save', function(ctx, next) {
+    var fimEm = (ctx.instance.fimEm).getTime();
+    var inicioEm = (ctx.instance.inicioEm).getTime();
+    ctx.instance.duracao = (fimEm - inicioEm) / (1000 * 60);
+    ctx.instance.valor = ctx.instance.duracao * (0.5);
+    next();
+});
+
+// Reservas.observe('before delete', function(ctx, next) {
+//     console.log(ctx.model.id.status);
+//     ctx.model.id.status = 'cancelada';
+//     next();
+// });
 };
