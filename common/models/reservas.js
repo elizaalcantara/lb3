600b15cef0adaf0b3741f002f39b1c8fdcf8ae04
Eliza Alcantara <ealcantara@br.ibm.com>
2019-04-22 15:27:59 -0300

    nao deleta reserva

--- common/models/reservas.js
+++ common/models/reservas.js
@@ -1,18 +1,28 @@
 /* eslint-disable indent */
 'use strict';
 
-module.exports = function(Reservas) {
-Reservas.observe('before save', function(ctx, next) {
-    var fimEm = (ctx.instance.fimEm).getTime();
-    var inicioEm = (ctx.instance.inicioEm).getTime();
-    ctx.instance.duracao = (fimEm - inicioEm) / (1000 * 60);
-    ctx.instance.valor = ctx.instance.duracao * (0.5);
-    next();
-});
+var statusId;
+var atualizarId;
 
-// Reservas.observe('before delete', function(ctx, next) {
-//     console.log(ctx.model.id.status);
-//     ctx.model.id.status = 'cancelada';
-//     next();
-// });
-};
+module.exports = function(Reservas) {
+    Reservas.observe('before save', function(ctx, next) {
+        if (ctx.isNewInstance){
+            var fimEm = (ctx.instance.fimEm).getTime();
+            var inicioEm = (ctx.instance.inicioEm).getTime();
+            ctx.instance.duracao = (fimEm - inicioEm) / (1000 * 60);
+            ctx.instance.valor = ctx.instance.duracao * (0.5);
+
+            
+        }
+        next();
+    });
+    
+    Reservas.observe('before delete', function naoDeletar(ctx, next) {
+        var err = new Error("Não deletar. Mudar o status para 'cancelada'");
+        err.statusCode = 400;
+        statusId = ctx.where.id;
+        console.log(statusId);
+        next(err);
+    });
+    
+}
