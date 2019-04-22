85a4ea7f0c71a43778863bc563e193439ddc7284
Eliza Alcantara <ealcantara@br.ibm.com>
2019-04-22 17:13:57 -0300

    reservas nao cancelam corrigido

--- common/models/reservas.js
+++ common/models/reservas.js
@@ -11,18 +11,24 @@ module.exports = function(Reservas) {
             var inicioEm = (ctx.instance.inicioEm).getTime();
             ctx.instance.duracao = (fimEm - inicioEm) / (1000 * 60);
             ctx.instance.valor = ctx.instance.duracao * (0.5);
-
+            
             
         }
         next();
     });
     
-    Reservas.observe('before delete', function naoDeletar(ctx, next) {
-        var err = new Error("Não deletar. Mudar o status para 'cancelada'");
-        err.statusCode = 400;
-        statusId = ctx.where.id;
-        console.log(statusId);
-        next(err);
+    Reservas.observe('before delete', (ctx, next) => {
+        Reservas.findById(ctx.where.id, (err, instance) => {
+
+            var err = new Error();
+            err.statusCode = 400;
+            if (err) {
+                ctx.hookState.deletedModelInstance = instance;  
+                console.log(ctx.hookState.deletedModelInstance)  
+                return next(err);
+            }
+            next(ctx);
+        });
+
     });
-    
 }
