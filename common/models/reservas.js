bf253da515c8193d986876f1614521075868bfd7
Eliza Alcantara <ealcantara@br.ibm.com>
2019-04-24 16:37:15 -0300

    validacao mesmo inicio e mesmo final de horario

--- common/models/reservas.js
+++ common/models/reservas.js
@@ -2,16 +2,12 @@
 'use strict';
 
 module.exports = function(Reservas) {
+
+    var mesmoInicio = null;
+    var mesmoFinal = null;
     
     //console.log(Reservas.definition.properties);
     Reservas.disableRemoteMethod('deleteById', true);			// Removes (DELETE) /products/:id
-    
-    // Reservas.beforeRemote('softDelete', async function(ctx){
-    //     var now = new Date();
-    //     ctx.instance.canceladaEm = now;
-    //     var canceladaEm = now;
-    //     console.log(ctx.instance);  
-    // });
     
     Reservas.softDelete = function(id,callback) {
         var result = {
@@ -22,6 +18,7 @@ module.exports = function(Reservas) {
     };
     
     Reservas.observe('before save', function(ctx, next) {
+        
         if (ctx.isNewInstance){
             var fimEm = (ctx.instance.fimEm).getTime();
             var inicioEm = (ctx.instance.inicioEm).getTime();
@@ -30,13 +27,29 @@ module.exports = function(Reservas) {
             var horaInteira = inicioEm % 10000;
             var duracaoMultiplo60 = ctx.instance.duracao % 60;
             console.log(horaInteira);
+            
+            Reservas.find({where:{inicioEm:ctx.instance.inicioEm}}, function(err,data){
+                mesmoInicio = data;
+                console.log("mesmo inicio"+ JSON.stringify(mesmoInicio))}
+            );
+            Reservas.find({where:{fimEm:ctx.instance.fimEm}}, function(err,data){
+                mesmoFinal = data;
+                console.log("mesmo final" + JSON.stringify(mesmoFinal))}
+            );
+            
             if (duracaoMultiplo60 !== 0 || horaInteira !== 0) {
                 var err = new Error();
                 err.statusCode = 422;
                 err.message = "tem menos de 60 min de duracao ou nao comeca em hora inteira.";
                 return next(err);
+            } else if (mesmoInicio !== null || mesmoFinal !== null) {
+                var err = new Error();
+                err.statusCode = 422;
+                err.message = "Horário indisponível.";
+                return next(err);
             }
-        }
-        next();
-    });
-}
+            next();
+        };
+        });
+    }
+    
