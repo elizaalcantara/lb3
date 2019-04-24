e88d06cae821164fbb24af7c84b29447af20a108
elizaal <elizaalcantara@poli.ufrj.br>
2019-04-24 00:46:20 -0300

    metodo delete refeito, mas falta persistir a alteracao nas propriedades

--- common/models/reservas.js
+++ common/models/reservas.js
@@ -2,7 +2,29 @@
 'use strict';
 
 module.exports = function(Reservas) {
-
+    
+    //console.log(Reservas.definition.properties);
+    Reservas.disableRemoteMethod('deleteById', true);			// Removes (DELETE) /products/:id
+    
+    Reservas.beforeRemote('prototype.softDelete', async function(ctx){
+        var now = new Date();
+        ctx.instance.canceladaEm = now;
+        var canceladaEm = now;
+    //     var properties = Reservas.definition.properties;
+    //     var id = ctx.instance.id;
+    //     Reservas.find({where:{id:id}}, function(err,result){
+    //     properties.push(canceladaEm)
+    // })
+        console.log(ctx.instance);  
+    });
+    
+    Reservas.prototype.softDelete = function(next) {
+        var result = {
+            status: "cancelada"
+        }
+        next(null,result);
+    };
+    
     Reservas.observe('before save', function(ctx, next) {
         if (ctx.isNewInstance){
             var fimEm = (ctx.instance.fimEm).getTime();
@@ -19,25 +41,25 @@ module.exports = function(Reservas) {
         next();
     });
     
-    Reservas.observe('before delete', (ctx, next) => {
-        
-        var now = new Date();
-        var alterarStatus = ctx.where.id;
-        console.log(alterarStatus);
-     
-        Reservas.findById(ctx.where.id, (err, instance) => {
-            var err = new Error();
-            err.statusCode = 400;
-            err.message = "Reservas não devem ser deletadas. Alterar o status pra cancelada."
-            instance.canceladaEm = now;
-            if (err) {
-                ctx.hookState.deletedModelInstance = instance;  
-                instance.canceladaEm = now;
-                console.log(ctx.hookState.deletedModelInstance);
-                return next(err);
-            }
-            next(ctx);
-        });
-    });
-
+    // Reservas.observe('before delete', (ctx, next) => {
+    
+    //     var now = new Date();
+    //     var alterarStatus = ctx.where.id;
+    //     console.log(alterarStatus);
+    
+    //     Reservas.findById(ctx.where.id, (err, instance) => {
+    //         var err = new Error();
+    //         err.statusCode = 400;
+    //         err.message = "Reservas não devem ser deletadas. Alterar o status pra cancelada."
+    //         instance.canceladaEm = now;
+    //         if (err) {
+    //             ctx.hookState.deletedModelInstance = instance;  
+    //             instance.canceladaEm = now;
+    //             console.log(ctx.hookState.deletedModelInstance);
+    //             return next(err);
+    //         }
+    //         next(ctx);
+    //     });
+    // });
+    
 }
